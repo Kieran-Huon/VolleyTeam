98951e622a1112a96909e72342b9e53ccdc0321a
Kess <kieran.huon@gmail.com>
2026-03-09 15:45:52 +0100

    app v2

--- script.js
+++ script.js
@@ -1,3 +1,88 @@
+// let players = []
+// let selectedPlayers = []
+
+// fetch('players.json')
+// .then(res => res.json())
+// .then(data => {
+// players = data
+// console.log(players)
+// displayPlayers()
+// })
+
+// function displayPlayers(){
+
+// const container = document.getElementById("playersList")
+
+// players.forEach((p,i)=>{
+
+// const div = document.createElement("div")
+// div.className="player"
+
+// div.innerHTML = `
+// <label>
+// <input type="checkbox" value="${i}">
+// ${p.name} (${p.poste})
+// </label>
+// `
+
+// container.appendChild(div)
+
+// })
+
+// }
+
+// document.getElementById("generateBtn").onclick = ()=>{
+
+// selectedPlayers = []
+
+// document.querySelectorAll("input[type=checkbox]:checked").forEach(c=>{
+// selectedPlayers.push(players[c.value])
+// })
+
+// generateTeams()
+
+// document.getElementById("regenBtn").style.display="block"
+
+// }
+
+// document.getElementById("regenBtn").onclick = generateTeams
+
+// function generateTeams(){
+
+// const teamsContainer = document.getElementById("teams")
+// teamsContainer.innerHTML=""
+
+// let shuffled = [...selectedPlayers].sort(()=>Math.random()-0.5)
+
+// let teamCount = 2
+
+// if(selectedPlayers.length >= 12) teamCount = 4
+// if(selectedPlayers.length >= 18) teamCount = 6
+
+// let teams = Array.from({length:teamCount}, ()=>[])
+
+// shuffled.forEach((player,i)=>{
+// teams[i % teamCount].push(player)
+// })
+
+// teams.forEach((team,i)=>{
+
+// const div = document.createElement("div")
+// div.className="team"
+
+// div.innerHTML = `<h2>Equipe ${i+1}</h2>`
+
+// team.forEach(p=>{
+// div.innerHTML += `<p>${p.name} (${p.poste})</p>`
+// })
+
+// teamsContainer.appendChild(div)
+
+// })
+
+
+// }
+
 let players = []
 let selectedPlayers = []
 
@@ -5,13 +90,13 @@ fetch('players.json')
 .then(res => res.json())
 .then(data => {
 players = data
-console.log(players)
 displayPlayers()
 })
 
 function displayPlayers(){
 
 const container = document.getElementById("playersList")
+container.innerHTML=""
 
 players.forEach((p,i)=>{
 
@@ -19,9 +104,10 @@ const div = document.createElement("div")
 div.className="player"
 
 div.innerHTML = `
-<label>
+<label class="player-label">
 <input type="checkbox" value="${i}">
-${p.name} (${p.poste})
+<span>${p.name}</span>
+<span class="poste">${p.poste}</span>
 </label>
 `
 
@@ -39,6 +125,11 @@ document.querySelectorAll("input[type=checkbox]:checked").forEach(c=>{
 selectedPlayers.push(players[c.value])
 })
 
+if(selectedPlayers.length < 4){
+alert("Pas assez de joueurs")
+return
+}
+
 generateTeams()
 
 document.getElementById("regenBtn").style.display="block"
@@ -47,6 +138,30 @@ document.getElementById("regenBtn").style.display="block"
 
 document.getElementById("regenBtn").onclick = generateTeams
 
+
+function calculateTeams(playerCount){
+
+const possibleTeams = [2,4,6,8]
+
+let bestTeams = 2
+let bestSize = 0
+
+possibleTeams.forEach(t=>{
+
+let size = playerCount / t
+
+if(size >= 4 && size > bestSize){
+bestTeams = t
+bestSize = size
+}
+
+})
+
+return bestTeams
+
+}
+
+
 function generateTeams(){
 
 const teamsContainer = document.getElementById("teams")
@@ -54,10 +169,7 @@ teamsContainer.innerHTML=""
 
 let shuffled = [...selectedPlayers].sort(()=>Math.random()-0.5)
 
-let teamCount = 2
-
-if(selectedPlayers.length >= 12) teamCount = 4
-if(selectedPlayers.length >= 18) teamCount = 6
+let teamCount = calculateTeams(selectedPlayers.length)
 
 let teams = Array.from({length:teamCount}, ()=>[])
 
@@ -65,20 +177,22 @@ shuffled.forEach((player,i)=>{
 teams[i % teamCount].push(player)
 })
 
+
 teams.forEach((team,i)=>{
 
 const div = document.createElement("div")
 div.className="team"
 
-div.innerHTML = `<h2>Equipe ${i+1}</h2>`
+let html = `<h2>Equipe ${i+1}</h2>`
 
 team.forEach(p=>{
-div.innerHTML += `<p>${p.name} (${p.poste})</p>`
+html += `<div class="playerTeam">${p.name} <span>${p.poste}</span></div>`
 })
+
+div.innerHTML = html
 
 teamsContainer.appendChild(div)
 
 })
-
 
 }
